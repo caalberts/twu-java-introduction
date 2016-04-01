8b8a15db47eebba4367fa2da9cdd856660fc2980
Albert Salim <albertlimca@gmail.com>
2016-04-01 16:19:25 +0800

    :triangle:

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by albert on 1/4/16.
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    Triangle a = new Triangle();
+    Triangle.drawHorizontalLine(8);
+  }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,28 @@
+/**
+ * Created by albert on 1/4/16.
+ */
+public class Triangle {
+  public void drawAsterisk () {
+    System.out.println("*");
+  }
+
+  public static void drawHorizontalLine (int n) {
+    for (int i = 0; i < n; i++) {
+      System.out.print("*");
+    }
+    System.out.print("\n");
+  }
+
+  public static void drawVerticalLine (int n) {
+    for (int i = 0; i < n; i++) {
+      System.out.println("*");
+    }
+  }
+
+  public static void drawTriangle(int n) {
+    for (int i = 0; i < n; i++) {
+      drawHorizontalLine(n);
+    }
+  }
+
+}
